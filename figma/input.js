9fa0fb0804598e8ff0617f08bcb1c3135d1dbbb0
WAHAB ALI <154264036+Wahabkhali@users.noreply.github.com>
2024-03-31 23:37:48 +0500

    Add files via upload

---
+++ figma/input.js
@@ -0,0 +1,16 @@
+
+ let userInput = document.getElementById("exampleInputEmail1").value;
+ let usertask = document.getElementById("exampleInputPassword1").value;   
+
+
+
+
+function input(){
+
+event.preventDefault();
+    let userInput = document.getElementById("exampleInputEmail1").value ;
+    let usertask = document.getElementById("exampleInputPassword1").value ;
+    let input = document.querySelector("#hard")
+console.log("userTask.value")
+
+}
