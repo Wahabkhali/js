4821784372c3018907e5efdf1ff02736a10579e7
WAHAB ALI <154264036+Wahabkhali@users.noreply.github.com>
2024-05-01 10:55:15 +0100

    Add files via upload

---
+++ capter 1/app.js
@@ -0,0 +1,19 @@
+
+// Q1Write a script to greet your website visitor using JS alert
+// box.
+// alert("java script")
+
+// Q2Write a script to display following message on your web
+// page:
+// alert("enter email addres:")
+// Q3Write a script to display following message on your web
+// page: (Hint : Use line break)
+// alert("welcome to js land.\nhappy coding")
+// Q4Write a script to display following messages in sequence:
+// window.alert("welcome to js land")
+// window.alert("Happy coding!")
+// Q5Generate the following message through browser’s
+// developer console:
+// alert("Hello... i can js through my web browser console")
+//  console.log(alert);
+// alert("using html and css")
