3e7777ba1fd9acf44a215beeb8a3f94a5a41c509
WAHAB ALI <154264036+Wahabkhali@users.noreply.github.com>
2024-05-02 08:08:24 +0100

    Add files via upload

---
+++ chapter 4/app.js
@@ -0,0 +1,35 @@
+//Q) Declare 3 variables in one statement.
+
+var visitorName = "", productTitle = "", quantity = "";
+
+// Q)Declare 5 legal & 5 illegal variable names.
+
+// Legal variable names:
+
+// firstName
+// productPrice
+// order_quantity
+// user123
+// _totalAmount
+
+// Illegal variable names:
+
+// 123user (Cannot start with a number);
+// product-price (Cannot contain hyphens);
+// order quantity (Cannot contain spaces);
+// total amount$ (Cannot contain special characters except underscores);
+// break (Reserved keyword in JavaScript);
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+document.write(` Variable names can only contain number,$,and _.For example $my_1stVariable <br/> 
+Variables must begin with a latter, $, or
+ _. For example $name, _name or name <br/>  Variable names are case sensitive <br/> Variable names should not be JS keyword `);
